45be42502b94998efbe8d2b159b658f5afac98fa
pritish <pritishkumar28@gmail.com>
2025-12-17 21:46:01 +0530

    login middleware & check is login or not and store req.originalUrl(past requested full url and also store in local to redirect on it

---
+++ middleware.js
@@ -0,0 +1,18 @@
+
+module.exports.isLoggedin = (req, res, next) => {
+    if (!req.isAuthenticated()) {
+        req.session.redirectUrl = req.originalUrl;
+        req.flash("error", "you must be logged in to action");
+        return res.redirect("/login");
+        
+    };
+    next();
+}
+
+
+module.exports.saveRedirectUrl = (req, res, next) => {
+    if(req.session.redirectUrl){
+        res.locals.redirectUrl = req.session.redirectUrl;
+        }
+    next();
+}
