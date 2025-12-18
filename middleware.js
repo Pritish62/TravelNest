92d1aace65d4e5bd1bbdfc4b40a8f4cd234f9d2c
pritish <pritishkumar28@gmail.com>
2025-12-18 23:51:22 +0530

    authorization for owner and review auther ,iddleware

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
-
+const Listing = require("./models/listing.js");
+const Review = require("./models/review.js");
 module.exports.isLoggedin = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
@@ -14,5 +15,25 @@ module.exports.saveRedirectUrl = (req, res, next) => {
     if(req.session.redirectUrl){
         res.locals.redirectUrl = req.session.redirectUrl;
         }
+    next();
+}
+
+module.exports.isOwner = async (req, res, next)=>{
+     const { id } = req.params;
+    const listing = await Listing.findById(id);
+    if(!listing.owner.equals(res.locals.currUser._id)){
+        req.flash("error", "you don't have permission to this action");
+        return res.redirect(`/listings/${id}`);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next)=>{
+     const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if(!review.author.equals(res.locals.currUser._id)){
+        req.flash("error", "you are not auther of the review");
+        return res.redirect(`/listings/${id}`);
+    }
     next();
 }
