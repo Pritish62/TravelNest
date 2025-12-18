1a136ad79ed81fd753b812b55862dcff5a6e8e62
pritish <pritishkumar28@gmail.com>
2025-12-18 23:54:43 +0530

    integrate authorization for review author

--- routes/review.js
+++ routes/review.js
@@ -5,6 +5,7 @@ const AppError = require("../utils/appError.js");
 const { reviewSchema} = require("../schema.js");
 const Listing = require("../models/listing.js");
 const Review = require("../models/review.js");
+const { isLoggedin, isReviewAuthor } = require("../middleware.js");
 
 
 const validateReview = (req, res, next) => {
@@ -19,11 +20,11 @@ const validateReview = (req, res, next) => {
 
 //-----Review------
 //post review  route
-router.post("/", validateReview,wrapAsync( async (req, res) => {
+router.post("/reviews",isLoggedin,validateReview,wrapAsync( async (req, res) => {
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
+    newReview.author = req.user._id;
     listing.reviews.push(newReview);
-
     await newReview.save();
     await listing.save();
     console.log("review saved");
@@ -31,7 +32,7 @@ router.post("/", validateReview,wrapAsync( async (req, res) => {
     res.redirect(`/listings/${listing._id}`);
 }));
 //delete review route
-router.delete("/:reviewId", wrapAsync(async(req, res)=>{
+router.delete("/reviews/:reviewId",isLoggedin,isReviewAuthor, wrapAsync(async(req, res)=>{
     let {id, reviewId} = req.params;
     await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
