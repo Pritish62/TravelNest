a0937042179a2b4f53d876fbf116a0931e73443b
pritish <pritishkumar28@gmail.com>
2025-12-23 19:17:54 +0530

    geolocating of coordinates

--- controller/listing.js
+++ controller/listing.js
@@ -1,52 +1,84 @@
 const Listing = require("../models/listing.js")
+const axios = require("axios");
 
 module.exports.index = async (req, res) => {
-    const allListings = await Listing.find({});
-    res.render("listings/index.ejs", { allListings });
+  const allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 }
-module.exports.renderNewFrom  =  (req, res) => {
-    res.render("listings/new.ejs");
+module.exports.renderNewFrom = (req, res) => {
+  res.render("listings/new.ejs");
 }
 
-module.exports.submitFromData =  async (req, res, next) => {
-    const url = req.file.path;
-    const filename = req.file.filename;
+module.exports.submitFromData = async (req, res, next) => {
 
-    let newListing = new Listing(req.body.listing);
-    newListing.owner = req.user._id;
-    newListing.image = {url, filename};
-    await newListing.save();
-    console.log(newListing);
-    req.flash("success", "new listings is created succesfully!");
-    res.redirect("/listings")
-    
+  const location = req.body.listing.location;
+
+  const response = await axios.get(
+    "https://nominatim.openstreetmap.org/search",
+    {
+      params: {
+        q: location,
+        format: "json",
+        limit: 1
+      }
+    }
+  );
+  if (!response.data.length) {
+    req.flash("error", "Invalid location");
+    return res.redirect("/listings/new");
+  }
+
+  const lat = response.data[0].lat;
+  const lng = response.data[0].lon;
+
+  const url = req.file.path;
+  const filename = req.file.filename;
+
+  let newListing = new Listing(req.body.listing);
+  newListing.owner = req.user._id;
+  newListing.image = { url, filename };
+  newListing.geometry = {
+    type: "Point",
+    coordinates: [lng, lat] // GeoJSON order
+  };
+  await newListing.save();
+  console.log(newListing);
+  req.flash("success", "new listings is created succesfully!");
+  res.redirect("/listings")
+
 };
 
 module.exports.showListing = async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id)
+  const { id } = req.params;
+  const listing = await Listing.findById(id)
     .populate({
-    path: 'reviews',
-    populate: {
-      path: 'author'
-    }
-  })
+      path: 'reviews',
+      populate: {
+        path: 'author'
+      }
+    })
     .populate("owner");
-    if(!listing){
+  if (!listing) {
     req.flash("error", "listing does not exist");
     res.redirect("/listings");
-    }
-    res.render("listings/show.ejs", { listing });
+  }
+  if (!listing.geometry) {
+    listing.geometry = {
+      type: "Point",
+      coordinates: [77.2090, 28.6139]
+    };
+  }
+  res.render("listings/show.ejs", { listing });
 }
 
-module.exports.renderEditFrom =  async (req, res) => {
-    const { id } = req.params;
-    const listing = await Listing.findById(id);
-    if(!listing){
+module.exports.renderEditFrom = async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  if (!listing) {
     req.flash("error", "listing does not exist");
     res.redirect("/listings");
-    }
-    res.render("listings/edit.ejs", { listing })
+  }
+  res.render("listings/edit.ejs", { listing })
 }
 
 module.exports.updateListing = async (req, res) => {
@@ -75,9 +107,9 @@ module.exports.updateListing = async (req, res) => {
 
 
 module.exports.destroyListing = async (req, res) => {
-    const { id } = req.params;
-    const deletedListing = await Listing.findByIdAndDelete(id);
-    console.log(deletedListing)
-    req.flash("success", "listing is deleted!");
-    res.redirect("/listings")
+  const { id } = req.params;
+  const deletedListing = await Listing.findByIdAndDelete(id);
+  console.log(deletedListing)
+  req.flash("success", "listing is deleted!");
+  res.redirect("/listings")
 }
