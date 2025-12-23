708afa44ca6626c64561206250f61456cd9cb8bd
pritish <pritishkumar28@gmail.com>
2025-12-23 23:48:38 +0530

    update middleware dor search bar logic

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ app.use((req, res, next) => {
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
     res.locals.currUser = req.user;
+    res.locals.isHome = req.path === "/listings";
     next();
 });
 
