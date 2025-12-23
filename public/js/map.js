edf414fabb3510dc8d7d2eab55b845b6b76928bf
pritish <pritishkumar28@gmail.com>
2025-12-23 19:22:39 +0530

    marker in map

--- public/js/map.js
+++ public/js/map.js
@@ -1,4 +1,4 @@
-const [lng, lat] = coordinates;
+const [lng, lat] = geometry.coordinates;
 const map = L.map("map").setView([lat, lng], 10);
 
 const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
@@ -8,9 +8,8 @@ const attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">Ope
 const tileLayer = L.tileLayer(tileUrl, {attribution});
 tileLayer.addTo(map);
 
-const marker = L.marker([lat, lng]);
-marker.addTo(map);
-
-
+L.marker([lat, lng])
+  .addTo(map)
+  .bindPopup("Listing location");
 
 
