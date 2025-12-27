623cd129a6511bde7962af988d4828577d8fc92e
pritish <pritishkumar28@gmail.com>
2025-12-27 23:21:44 +0530

    home route updated

--- app.js
+++ app.js
@@ -80,6 +80,10 @@ app.use(methodOverride("_method"))
 
 
 //use of routes
+app.get("/", (req, res) => {
+  res.render("home"); // later
+});
+
 app.use("/listings", listingsRoute )
 app.use("/listings/:id", reviewsRoute)
 app.use("/", usersRoute);
