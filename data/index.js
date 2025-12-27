8e35a86abfd6676ce190bb3339b97c6381feb73b
pritish <pritishkumar28@gmail.com>
2025-12-27 22:55:55 +0530

    demo data added  in mongodb server

--- data/index.js
+++ data/index.js
@@ -1,17 +1,27 @@
+const path = require("path");
+require("dotenv").config({
+  path: path.resolve(__dirname, "../.env")
+});
+
 const mongoose = require("mongoose");
 const initData = require("./init.js");
 const Listing = require("../models/listing.js");
 
-
-main().catch(err => console.log(err));
-
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/TravelNest');
+  try {
+    await mongoose.connect(process.env.MONGO_ATLAS);
+    console.log("MongoDB Atlas connected");
+
+    await seedDB();
+
+    mongoose.connection.close();
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-const initDB = async () => {
+async function seedDB() {
   await Listing.deleteMany({});
-
   initData.data = initData.data.map((obj) => ({
     ...obj,
     owner: "69417ad4239778d81c870831",
@@ -19,10 +29,29 @@ const initDB = async () => {
       type: "Point",
       coordinates: [77.2090, 28.6139]
     }
-  }));
+  
+}));
+await Listing.insertMany(initData.data);
+  console.log("Demo data added");
+}
+main();
 
-  await Listing.insertMany(initData.data);
-  console.log("data inserted");
-};
+// const initDB = async () => {
+//   await Listing.deleteMany({});
 
-initDB();
+//   initData.data = initData.data.map((obj) => ({
+//     ...obj,
+//     owner: "69417ad4239778d81c870831",
+//     geometry: obj.geometry || {
+//       type: "Point",
+//       coordinates: [77.2090, 28.6139]
+//     }
+//   }));
+
+  // await Listing.insertMany(initData.data);
+//   console.log("data inserted");
+// };
+
+// initDB();
+
+
